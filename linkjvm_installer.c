9ee852c7013738c603287afe72dadb5e390b9c02
Markus Klein <m@mklein.co.at>
2014-01-28 16:55:09 +0100

    install linkjvm offline

--- linkjvm_installer.c
+++ linkjvm_installer.c
@@ -2,19 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-	printf("LinkJVM Installer v1.0\n(c)2013, Markus Klein\nMIT License");
-	return system(
-	"cd /tmp;\
-	echo 'Fetching latest LinkJVM package...';\
-	wget http://files.mklein.co.at/LinkJVM/packages/latest.ipk;\
-	echo 'Removing old java files...'\
+	printf("LinkJVM Installer v1.0\n(c)2013, Markus Klein\nMIT License\n");
+        return system(
+        "echo 'Removing old java packages...';\
 	opkg remove --force-depends jamvm jikes classpath classpath-dev classpath-common;\
-	echo 'Installing latest LinkJVM package...';\
-	opkg install latest.ipk;\
-	echo 'Adding environment variables...';\
-	wget http://files.mklein.co.at/LinkJVM/packages/base-files_3.0.14-r87_kovan.ipk;
-	opkg install base-files_3.0.14-r87_kovan.ipk;
-	echo 'Cleaning up...'
-	rm base-files_3.0.14-r87_kovan.ipk latest.ipk;
-	echo 'Done.';");
+        echo 'Installing latest LinkJVM package...';\
+        opkg install /kovan/media/sda1/LinkJVM/latest.ipk;\
+        echo 'Adding environment variables...';\
+        opkg install /kovan/media/sda1/LinkJVM/base-files_3.0.14-r87_kovan.ipk;\
+        echo 'Done.';");
 }
